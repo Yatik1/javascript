6210ded09987a35cde2ca244fc07bdb0185dfe7f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-10 16:50:18 +0530

    03

---
+++ basics/typeconversion.js
@@ -0,0 +1,27 @@
+// let v='yatik'
+// console.log(typeof v) //string  
+
+// let vNumber = Number(v)
+// console.log(typeof vNumber)  // number
+// console.log(vNumber);       //Nan 
+
+
+let v=31
+let vNumber = String(v) 
+console.log(typeof vNumber)  // string
+console.log(vNumber);       //string  
+
+
+let name='yatik'
+console.log(typeof name);
+
+const boolValue = Boolean(name)
+console.log(typeof boolValue) //boolean
+console.log(boolValue);  //true
+
+const numBool = Number(boolValue)
+console.log(typeof numBool); // number
+console.log(numBool); // 1
+
+const fValue=false
+console.log(Number(fValue));  // 0 => type:Number 

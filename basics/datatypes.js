b91249e3a7f12003e4fff28999ee041b329a423f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-10 16:17:33 +0530

    02

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as per the new standards
+
+// Data Type
+
+let age=21;  //number
+let name='yatik' //string
+var isOpen = true //boolean 
+let foo;  //undefined 
+          // null
+          //BigInt
+          //symbol 
+          /* these above 7 data types are primitive data types and are immutable */
+
+console.table([typeof age , typeof name , typeof isOpen , typeof foo])
+console.log(typeof null)  // => object
+  
+  /* theres an another data type called object => {} */

252b68345a20193dcea148e19e5fe9ea082f22ed
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-10 17:21:55 +0530

    03

--- basics/typeconversion.js
+++ basics/typeconversion.js
@@ -25,3 +25,42 @@ console.log(numBool); // 1
 
 const fValue=false
 console.log(Number(fValue));  // 0 => type:Number 
+
+/*  operations  */
+
+let a=10;
+let b = a++;
+console.log(b);  //10
+console.log(a);  //11
+
+// let c=b++;
+// console.log(c); //10
+// console.log(b); //11 
+
+let c=++b;
+console.log(c); //11
+console.log(b); //11 
+
+
+console.log(2+2) //4
+console.log("2" + "2") //22
+
+console.log(+"2") //2 => num
+console.log(1+ +"8") //9
+console.log(1+"2"); //12
+console.log(1+2+"2"); //32
+console.log(1+2+"2"+3); //323
+
+console.log("2"+1) //21
+console.log(2+"1") //21
+console.log(2+3+"1") //51
+console.log(2+3+"1"+1) //511
+
+console.log(true)
+console.log(+true) //1
+console.log(+false) //0  
+
+const s1='Hello' , s2=' World'
+const s3 = s1+s2
+console.log(s3); //Hello World
+

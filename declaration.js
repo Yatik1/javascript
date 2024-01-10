4b2951714b949e7e82782223973e46ab4200bdd5
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-10 15:40:36 +0530

    declaration

---
+++ declaration.js
@@ -0,0 +1,9 @@
+const age=21
+let name='yatik'
+var rollno = '34'  //prefer not to use var because of issue in block scoping
+city='Delhi'
+
+let address;
+                               
+console.table([name,age,rollno,city,address]);
+

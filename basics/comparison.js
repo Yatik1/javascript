0b7186af6e24ae90d8539c4454ee8f9e88e292c0
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-12 23:49:30 +0530

    04

--- basics/comparison.js
+++ basics/comparison.js
@@ -1,12 +1,12 @@
 console.log("2" == 2); // --> true : just check value  
 console.log("2" === 2); // --> false : check both value and type of value , also called strict equality 
 
-// console.log(2>1);
-// console.log(2==1);
-// console.log(2>=1);
+console.log(2>1); //--> true
+console.log(2==1); //--> false
+console.log(2>=1); //--> true
 
-// console.log("2" > 1);
-// console.log("02" < 1);
+console.log("2" > 1); //--> true
+console.log("02" < 1); //--> false
 
 // the working of equality operator and comaprison operator are different , the comparison op. >,<,>=,<= converts the null to 0
 console.log(null > 0); // --> false  

ae22ad1499b4a9998674cedc9eea6eb1eea32a01
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-20 17:49:02 +0530

    01

--- basics3/function1.js
+++ basics3/function1.js
@@ -2,24 +2,24 @@ function myName() {
     console.log("Yatik");
 }
  
-// myName()   // --> Yatik
+myName()   // --> Yatik
 
-// function add(n1,n2) {
-//     console.log(n1+n2)
-// }
+function add(n1,n2) {
+    console.log(n1+n2)
+}
 
-// add(2,3) // --> 5
+add(2,3) // --> 5
 
-// const result = add(4,3)
-// console.log(result);  // --> undefined :  as add() doesn't returning anything ! 
+const result = add(4,3)
+console.log(result);  // --> undefined :  as add() doesn't returning anything ! 
 
 function add(n1,n2) {
     return n1+n2
     console.log("Hurry")  //--> after return in function , no code will execute inside the same function , but if its before return it'll executes 
 }
 
-const result = add(41,3)
-console.log(result); // --> 44
+const resultNew = add(41,3)
+console.log(resultNew); // --> 44
 
 
 function User(username) {

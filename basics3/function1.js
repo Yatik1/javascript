3240b2a9d42ebffa491664a88287e7e3ead8e8e3
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-21 16:17:56 +0530

    01

--- basics3/function1.js
+++ basics3/function1.js
@@ -1,51 +1,51 @@
-// function myName() {
-//     console.log("Yatik");
-// }
+function myName() {
+    console.log("Yatik");
+}
  
-// myName()   // --> Yatik
+myName()   // --> Yatik
 
-// function add(n1,n2) {
-//     console.log(n1+n2)
-// }
+function add(n1,n2) {
+    console.log(n1+n2)
+}
 
-// add(2,3) // --> 5
+add(2,3) // --> 5
 
-// const result = add(4,3)
-// console.log(result);  // --> undefined :  as add() doesn't returning anything ! 
+const result = add(4,3)
+console.log(result);  // --> undefined :  as add() doesn't returning anything ! 
 
-// function add(n1,n2) {
-//     return n1+n2
-//     console.log("Hurry")  //--> after return in function , no code will execute inside the same function , but if its before return it'll executes 
-// }
+function add(n1,n2) {
+    return n1+n2
+    console.log("Hurry")  //--> after return in function , no code will execute inside the same function , but if its before return it'll executes 
+}
 
-// const resultNew = add(41,3)
-// console.log(resultNew); // --> 44
-
-
-// function User(username) {
-//     return `Welcome! ${username}`
-// }
-
-// console.log(User("yatik")); // --> Welcome! yatik 
-
-// const greet = User("mayank")
-// console.log(greet); // --> Welcome! mayank
+const resultNew = add(41,3)
+console.log(resultNew); // --> 44
 
 
-// function loginUserMessage(username){
-//     if(!username){
-//         console.log("Please enter a username");
-//         return
-//     }
-//     return `${username} just logged in`
-// }
+function User(username) {
+    return `Welcome! ${username}`
+}
 
-// console.log(loginUserMessage("Rohan")) // --> Rohan just logged in
-// console.log(loginUserMessage()) 
-// /* 
-// Please enter a username
-// undefined
-//  */
+console.log(User("yatik")); // --> Welcome! yatik 
+
+const greet = User("mayank")
+console.log(greet); // --> Welcome! mayank
+
+
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Rohan")) // --> Rohan just logged in
+console.log(loginUserMessage()) 
+/* 
+Please enter a username
+undefined
+ */
 
 /* ------------------------------------------------------- */
 

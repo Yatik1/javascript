335b8e691dd1700786ed3d07fa488fc09d99f92c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-02-01 23:15:14 +0530

    03

--- control_flow/truthy-falsy.js
+++ control_flow/truthy-falsy.js
@@ -41,3 +41,10 @@ console.log(val1);
 
 const iceTeaPrice = 100
 iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
+false == 0 // --> true
+0=="" // --> true
+false == "" // --> true
+
+

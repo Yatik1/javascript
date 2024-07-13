7e0d81aa73da5ee2bb82a6cbdb84391d856c3b79
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-13 22:59:21 +0530

    01

---
+++ 30d_challenges/01.js
@@ -0,0 +1,47 @@
+// Activity 1 : Variable Declaration
+
+var num =1
+console.log(num);
+
+let str = "Hello World ! "
+console.log(str);
+
+// Activity 2 : Constant Declaration
+
+const isValue = true
+console.log(isValue)
+
+// Activity 3 : Data Types
+
+let n = 2
+console.log(n ,typeof n)
+
+let name = "My name is Yatik"
+console.log(name , typeof name)
+
+let isAdult = true
+console.log(isAdult , typeof isAdult)
+
+let myObj = {
+    name : "Yatik" , 
+    state : "Dehi"
+}
+console.log(myObj , typeof myObj)
+
+let arr = [1,2,3,4]
+console.log(arr , typeof arr)
+
+// Activity 4 : Reassiging Variables.
+
+let price = 3939
+console.log(price)
+price = 400
+console.log(price)
+
+// Activity 5 : Understanding const
+
+const isOkay = false
+console.log(isOkay)
+
+isOkay = true
+console.log(isOkay) // -- > error : Assignment to constant variable.
